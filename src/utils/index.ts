e6af49255941f69693b9910856f9933a347f1b33
Wik3d <61479652+wik3d@users.noreply.github.com>
2024-01-04 23:14:05 +0000

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -35,7 +35,7 @@ export const shortcodeFromMediaID = (media_id: string) => {
 };
 
 /** Instagram post regex */
-const IGPostRegex = /(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/(?:p|reel|tv)\/[\w-]+|(?:stories|instagram\.com)\/[\w-]+\/(?:\d+|p\/[\w-]+))(?:\/)?(\?.*)?$/gim;
+export const IGPostRegex = /(?:https?:\/\/)?(?:www\.)?(?:instagram\.com\/(?:p|reel|tv)\/[\w-]+|(?:stories|instagram\.com)\/[\w-]+\/(?:\d+|p\/[\w-]+))(?:\/)?(\?.*)?$/gim;
 
 /**
  * format instagram long url to get shortcode
